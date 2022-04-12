cebf7f88f696661c1227e51a856739af15cb7771
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-04-11 23:19:42 -0300

    rendered component ToastContainer

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Home } from "./pages/Home";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./styles/global.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
+    <ToastContainer />
   </React.StrictMode>,
   document.getElementById("root")
 );
